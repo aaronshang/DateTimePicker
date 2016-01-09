525343a8182e58eb684df0f44211dcc57b876389
skwaityou <skwaityou@sina.com>
2016-01-09 23:26:08 +0800

    Add UISwitch ..

--- KDataTimePageDemo/KDataTimePageDemo/KDateTime/KDateTimeViewController.m
+++ KDataTimePageDemo/KDataTimePageDemo/KDateTime/KDateTimeViewController.m
@@ -11,6 +11,9 @@
 @interface KDateTimeViewController()<UITableViewDataSource, UITableViewDelegate>
 
 @property (strong, nonatomic) NSArray *mLeftLabelNameAry;
+@property (strong, nonatomic) UIDatePicker *mDatePicker;
+@property (strong, nonatomic) NSDate *mCurrentDate;
+@property (strong, nonatomic) UISwitch *mSwitch;
 
 @end
 
@@ -25,13 +28,55 @@
     
     self.mTableView.delegate = self;
     self.mTableView.dataSource = self;
+    
+    self.mDatePicker = [[UIDatePicker alloc] init];
+    self.mDatePicker.datePickerMode = UIDatePickerModeDateAndTime;
+    self.mCurrentDate = [NSDate date];
+    
+    [self.mDatePicker addTarget:self action:@selector(PickerValueChanged:) forControlEvents:UIControlEventValueChanged];
+    
+    self.mSwitch = [[UISwitch alloc] init];
+    [self.mSwitch addTarget:self action:@selector(SwithValueChanged:) forControlEvents:UIControlEventValueChanged];
+    self.mSwitch.on = NO;
 }
 
+- (NSString *) getDateStrWithDate:(NSDate *) date withHaveHourMin:(BOOL) have{
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    
+    NSString *pattern = (have)?@"yyyy-MM-dd HH:mm":@"yyyy-MM-dd";
+    [dateFormatter setDateFormat:pattern];
+    
+    NSString *currentDateStr = [dateFormatter stringFromDate:date];
+    NSLog(@"currentDateStr: %@", currentDateStr);
+    return currentDateStr;
+}
+
+- (void) PickerValueChanged:(id) sender{
+   
+    UIDatePicker* control = (UIDatePicker*)sender;
+    self.mCurrentDate = control.date;
+    [self.mTableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:1 inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
+}
+
+- (void) SwithValueChanged:(id) sender{
+    
+    UISwitch *switchControl = (UISwitch *)sender;
+    if (switchControl.on) {
+        self.mDatePicker.datePickerMode = UIDatePickerModeDate;
+    }else{
+        self.mDatePicker.datePickerMode = UIDatePickerModeDateAndTime;
+    }
+    
+    [self.mTableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:2 inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
+}
 
 #pragma mark-
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 44;
+    
+    CGFloat rvt = (indexPath.row==2)?216:44;
+    return rvt;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
@@ -52,11 +97,23 @@
     
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CELLIDIDENTITER];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
     }
     
+    cell.detailTextLabel.text = @"";
     cell.textLabel.text = [self.mLeftLabelNameAry objectAtIndex:indexPath.row];
-    cell.detailTextLabel.text = @"2014-1-1 08:09";
     
+    if (indexPath.row==0) {
+        cell.accessoryView = self.mSwitch;
+    }
+    else if(indexPath.row==1){
+        cell.detailTextLabel.text = [self getDateStrWithDate:self.mCurrentDate withHaveHourMin:!self.mSwitch.on];
+    }
+    else if(indexPath.row==2){
+        cell.textLabel.text = @"";
+        cell.detailTextLabel.text = @"";
+        [cell.contentView addSubview:self.mDatePicker];
+    }
     
     
     return  cell;
